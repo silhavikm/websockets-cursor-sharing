e49a48b5d6f85ca838a056bec17fec22f87ab075
silhavikm <136180591+silhavikm@users.noreply.github.com>
2024-04-22 16:00:01 +0200

    Update index.js
    update websocket port to 80

--- app/index.js
+++ app/index.js
@@ -14,7 +14,7 @@
     };
         
     async function connectToServer() {    
-        const ws = new WebSocket('ws://localhost:7071/ws');
+        const ws = new WebSocket('ws://localhost:80/ws');
         return new Promise((resolve, reject) => {
             const timer = setInterval(() => {
                 if(ws.readyState === 1) {
